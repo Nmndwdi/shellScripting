4be6cbb76c39233f279b10906cad5e5f9fcf6496
Ubuntu <ubuntu@ip-172-31-45-169.ap-south-1.compute.internal>
2024-03-03 14:41:22 +0000

    functions and basics are completed

---
+++ functions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function check_pokemon_type()
+{
+	#read -p "Enter you pokemon : " pokemon
+	# pokemon=$1  # this will not give error but using local to declare                           # variable withing a function is a good practice
+
+	local pokemon=$1
+	local pok=$2
+	echo "$pok"
+
+	if [[ $pokemon == "charizard" ]]
+	then
+		echo "$pokemon is a fire type pokemon"
+	elif [[ $pokemon == "squirtle" ]];
+	then
+		echo "$pokemon is a water type pokemon"
+	elif [[ $pokemon == "balbasaur" ]];
+	then
+		echo "$pokemon is a grass type pokemon"
+	elif [[ $pokemon == "pikachu" ]]
+	then
+		echo "$pokemon is a electric type pokemon"
+	else
+		echo "type not found"
+	fi
+}
+
+read -p "Enter you pokemon : " pokemon
+
+check_pokemon_type "$pokemon" "gangar"
